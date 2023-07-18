566482b32ba2683881fcd47ecadeb95317f9e1cb
dli285 <139133657+dli285@users.noreply.github.com>
2023-07-18 21:22:06 +0500

    Add files via upload

---
+++ fe-15-js-empty/script.js
@@ -0,0 +1,32 @@
+//кнопки
+let inputFirst = document.querySelector('.calculator-input-first')
+let inputSecond = document.querySelector('.calculator-input-second')
+
+//кнопки
+let buttonPlus = document.querySelector('.calculator-plus')
+let buttonMinus = document.querySelector('.calculator-minus')
+let buttonEquals = document.querySelector('.calculator-equals')
+
+//Значения
+let valueOne = null
+let valueTwo = null
+let result = null
+
+buttonPlus.addEventListener('click', function() {
+    valueOne = inputFirst.value
+    valueTwo = inputSecond.value
+    
+    result = Number(valueOne) + Number(valueTwo)
+})
+
+buttonMinus.addEventListener('click', function() {
+    valueOne = inputFirst.value
+    valueTwo = inputSecond.value
+ 
+    result = Number(valueOne) - Number(valueTwo)
+})
+buttonEquals.addEventListener('click', function() {
+    alert('Результат:' + result)
+    inputFirst.value = ''
+    inputSecond.value = ''
+})
